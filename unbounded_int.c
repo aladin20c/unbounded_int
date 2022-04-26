143381a6f57ec88319057460825ce73163a734f9
cheniour <aladincheniour19@gmail.com>
2022-04-26 15:29:24 +0200

    first 4 qst of 4.1

---
+++ unbounded_int.c
@@ -0,0 +1,272 @@
+/*
+   fichier contenant les definitions des fonctions données dans polynomes.h
+   gcc unbounded_int.c -lm && ./a.out
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+#include "unbounded_int.h"
+
+
+static unbounded_int create_empty_unbounded_int();
+
+static chiffre *create_chiffre(char c);
+
+static void free_unbounded_int(unbounded_int ui);
+
+static void afficher_unbounded_int(unbounded_int ui);
+
+static int matchesnumber(const char* str);
+
+static unbounded_int insertLast(unbounded_int ui,char c);
+
+static unbounded_int insertFirst(unbounded_int ui,char c);
+
+
+
+/*---------------------------------------------MAIN---------------------------------------------*/
+
+
+
+static unbounded_int create_empty_unbounded_int(){
+  return (unbounded_int){.signe='*', .len=0, .premier=NULL, .dernier=NULL};
+}
+
+
+static chiffre *create_chiffre(char c){
+  chiffre *ch=malloc(sizeof(chiffre));
+  if(ch==NULL){
+    fprintf(stderr, "error in function create_chiffre(char,chiffre*,chiffre*)\n");
+    return NULL;
+  }
+  ch->c=c;
+  ch->suivant=NULL;
+  ch->precedent=NULL;
+  return ch;
+}
+
+
+static void free_unbounded_int(unbounded_int ui){
+    chiffre *tmp=ui.premier;
+    if(tmp==NULL){
+      return;
+    }
+    while (tmp!=NULL) {
+      chiffre *tmp2=tmp;
+      tmp=tmp->suivant;
+      free(tmp2);
+    }
+    ui.signe='*';
+    ui.premier=NULL;
+    ui.dernier=NULL;
+}
+
+
+static void afficher_unbounded_int(unbounded_int ui){
+  printf("%c",ui.signe);
+  chiffre *tmp=ui.premier;
+  while (tmp!=NULL) {
+    printf("%c",tmp->c);
+    tmp=tmp->suivant;
+  }
+  printf("\n");
+}
+
+
+static int matches_unbounded_int(const char* e){
+  if(strlen(e)==0) {return 0;}
+  if(e[0]!='+' && e[0]!='-' &&( e[0]<'0' || e[0]>'9')) {return 0;}
+
+  for (int i = 1; i < strlen(e); i++) {
+    if(e[i]<'0' || e[i]>'9'){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+
+static unbounded_int insertLast(unbounded_int ui,char c){
+  chiffre *ch=create_chiffre(c);
+  if(ch==NULL){
+    fprintf(stderr, "error in function insertLast(unbounded_int,char)\n");
+    return create_empty_unbounded_int();
+  }
+  if(ui.dernier==NULL){
+    ui.dernier=ch;
+    ui.premier=ch;
+    ch->suivant=ch;
+    ch->precedent=ch;
+  }else if(ui.premier==ui.dernier){
+    ch->suivant=NULL;
+    ch->precedent=ui.premier;
+    (ui.premier)->precedent=NULL;
+    (ui.premier)->suivant=ch;
+    ui.dernier=ch;
+  }else{
+    ch->suivant=NULL;
+    ch->precedent=ui.dernier;
+    (ui.dernier)->suivant=ch;
+    ui.dernier=ch;
+  }
+  ui.len+=1;
+  return ui;
+}
+
+static unbounded_int insertFirst(unbounded_int ui,char c){
+  chiffre *ch=create_chiffre(c);
+  if(ch==NULL){
+    fprintf(stderr, "error in function insertLast(unbounded_int,char)\n");
+    return create_empty_unbounded_int();
+  }
+  if(ui.premier==NULL){
+    ui.premier=ch;
+    ui.dernier=ch;
+    ch->suivant=ch;
+    ch->precedent=ch;
+  }else if(ui.premier==ui.dernier){
+    ch->suivant=ui.dernier;
+    ch->precedent=NULL;
+    (ui.dernier)->precedent=ch;
+    (ui.dernier)->suivant=NULL;
+    ui.premier=ch;
+  }else{
+    ch->suivant=ui.premier;
+    ch->precedent=NULL;
+    (ui.premier)->precedent=ch;
+    ui.premier=ch;
+  }
+  ui.len+=1;
+  return ui;
+}
+
+
+
+
+unbounded_int string2unbounded_int(const char *e){
+
+  unbounded_int ui = create_empty_unbounded_int();
+  if(! matches_unbounded_int(e)) return ui;
+  //fixing signe
+  int indice=0;
+  if(e[0]=='+' || e[0]=='-'){
+    ui.signe=e[0];
+    indice++;
+  }else{
+    ui.signe='+';
+  }
+  //initialising chiiffres
+  for (; indice < strlen(e); indice++) {
+    ui=insertLast(ui,e[indice]);
+    if(ui.signe=='*') return ui;
+  }
+  return ui;
+}
+
+
+
+unbounded_int ll2unbounded_int(long long i){
+  unbounded_int ui = create_empty_unbounded_int();
+  if(i<0){
+    ui.signe='-';
+    i*=-1;
+  }else{
+    ui.signe='+';
+  }
+  while (i) {
+    char c=(i % 10)+'0';
+    ui=insertFirst(ui,c);
+    if(ui.signe=='*') {return ui;}
+    i/=10;
+  }
+  return ui;
+}
+
+
+char *unbounded_int2string(unbounded_int ui){
+  if(ui.signe=='*') return NULL;
+  char * str= malloc(ui.len*sizeof(char)+2);
+  if(str==NULL){
+    return "0";
+  }
+  if(ui.signe=='-'){
+    str[0]='-';
+  }else{
+    str[0]='+';
+  }
+
+  int indice=1;
+  chiffre *tmp=ui.premier;
+  while (tmp!=NULL) {
+    str[indice]=tmp->c;
+    tmp=tmp->suivant;
+    indice++;
+  }
+  return str;
+}
+
+int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b){
+  if(a.signe=='*'){
+    if(b.signe=='*') return 0;
+    else return -1;
+  }
+  if(b.signe=='*'){
+    if(a.signe=='*') return 0;
+    else return 1;
+  }
+
+  int coeff=1;
+  if(a.signe!=b.signe){
+      if(a.signe=='+') return 1;
+      else return -1;
+  }else{
+      if(a.signe=='-') coeff=-1;
+  }
+
+  if(a.len>b.len){
+    return 1*coeff;
+  }else if(a.len<b.len){
+    return -1*coeff;
+  }else{
+    chiffre* tmp1=a.premier;
+    chiffre* tmp2=b.premier;
+    for (int i = 0; i < a.len; i++) {
+        if((tmp1->c)>(tmp2->c)){
+            return 1*coeff;
+        }else if((tmp1->c)<(tmp2->c)){
+            return -1*coeff;
+        }else{
+            tmp1=tmp1->suivant;
+            tmp2=tmp2->suivant;
+        }
+    }
+  }
+  return 0;
+}
+
+
+
+int unbounded_int_cmp_ll(unbounded_int a, long long b){
+  if(a.signe=='*'){
+    return -1;
+  }
+  unbounded_int a2= ll2unbounded_int(b);
+  return unbounded_int_cmp_unbounded_int(a,a2);
+}
+
+
+
+
+int main(int argc, char const *argv[]) {
+  unbounded_int ex1=string2unbounded_int("-93823876688");
+  unbounded_int ex2=ll2unbounded_int((long long)+93807);
+  afficher_unbounded_int(ex1);
+  afficher_unbounded_int(ex2);
+  printf("%d\n", unbounded_int_cmp_unbounded_int(ex1,ex2));
+  printf("%d\n", unbounded_int_cmp_ll(ex1,+9382388));
+  free_unbounded_int(ex1);
+  free_unbounded_int(ex2);
+}
---
+++ unbounded_int.h
@@ -0,0 +1,66 @@
+
+/*Les entiers seront représentés par des listes doublements chaînées
+Les champs precedent et suivant pointent respectivement vers le bloc précédent et suivant.*/
+
+typedef struct chiffre{
+  struct chiffre  *suivant;
+  char c;
+  struct chiffre *precedent;
+} chiffre;
+
+
+
+/*
+On utilisera la structure les pointeurs vers le premier et le dernier bloc de la liste de chiffre,
+le signe stocké comme un caractère, soit ’+’ soit ’-’,
+len - le nombre de chiffres sur la liste.
+*/
+
+typedef struct{
+  char signe; /* soit ’+’ soit ’-’ */
+  size_t len; /* longueur de la liste */
+  chiffre *premier; /* pointeur vers le premier élément de la liste*/
+  chiffre *dernier; /*pointeur vers le dernier élément de la liste*/
+} unbounded_int;
+
+
+
+
+
+/*qui prend en argument une chaîne de caractères et retourne la structure unbounded_int*/
+unbounded_int string2unbounded_int(const char *e);
+
+
+
+/*qui prend en argument un nombre long long et retourne la structure unbounded_int*/
+unbounded_int ll2unbounded_int(long long i);
+
+
+
+/*prend en argument une structure unbounded_int représentant un entier et retourne cet entier sous forme d’une chaîne de caractères.*/
+char *unbounded_int2string(unbounded_int i);
+
+
+
+/*qui re- tourne une des valeurs −1, 0, 1*/
+int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b);
+
+
+
+/*qui re- tourne une des valeurs −1, 0, 1*/
+int unbounded_int_cmp_ll(unbounded_int a, long long b);
+
+
+
+/*renvoyant la représentation de la somme de deux entiers représentés par a et b,*/
+unbounded_int unbounded_int_somme(unbounded_int a, unbounded_int b);
+
+
+
+/* ren- voyant la représentation de leur différence,*/
+unbounded_int unbounded_int_difference( unbounded_int a, unbounded_int b);
+
+
+
+/*renvoyant la représentation de leur produit,*/
+unbounded_int unbounded_int_produit( unbounded_int a, unbounded_int b);
